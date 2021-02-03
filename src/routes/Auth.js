714607617056626d13f0d65a31f2e17dbce1ac34
kim-hyunjin <hyunjin1612@gmail.com>
2021-02-03 22:03:27 +0900

    create account with email

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { authService } from 'myFirebase'
 
 const Auth = () => {
         const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
+        const [newAccount, setNewAccount] = useState(true);
         const onChange = (event) => {
                 const { target: { name, value } } = event;
                 if (name === 'email') {
@@ -11,8 +13,21 @@ const Auth = () => {
                         setPassword(value)
                 }
         };
-        const onSubmit = (event) => {
+        const onSubmit = async (event) => {
                 event.preventDefault();
+                try {
+                        let data;
+                        if (newAccount) {
+                                //create account
+                                data = authService.createUserWithEmailAndPassword(email, password)
+                        } else {
+                                //log in
+                                data = authService.signInWithEmailAndPassword(email, password)
+                        }
+                        console.log(data)
+                } catch (error) {
+                        console.log(error)
+                }
         };
 
         return (
@@ -20,7 +35,7 @@ const Auth = () => {
                         <form onSubmit={onSubmit}>
                                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                                <input type="submit" value="Log In" />
+                                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
                         </form>
                         <div>
                                 <button>Continue with Google</button>
