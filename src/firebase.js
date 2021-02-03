d38a5d9b0ec4cc4618f8398e9b4ae6ee4b71ea20
kim-hyunjin <hyunjin1612@gmail.com>
2021-02-03 20:50:47 +0900

    init and install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+        apiKey: "AIzaSyAlOEJ8_8IvXoCnkHYm6q2SXGUmw5wC8Qs",
+        authDomain: "kwitter-c2c94.firebaseapp.com",
+        projectId: "kwitter-c2c94",
+        storageBucket: "kwitter-c2c94.appspot.com",
+        messagingSenderId: "824087394375",
+        appId: "1:824087394375:web:d7898a117dfc5457261962"
+};
+
+export default firebase.initializeApp(firebaseConfig);
