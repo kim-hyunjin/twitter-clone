7d2506e03f9f0c0deb39382c17e337256b9a7cd7
kim-hyunjin <hyunjin1612@gmail.com>
2021-02-06 17:48:46 +0900

    edit profile, using firestore query, react state update

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,36 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUserObj(user);
+        // setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid:user.uid,
+          updateProfile: (args) => user.updateProfile(args)
+        });
       }
       setInit(true);
     });
-  }, [])
+  }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    // setUserObj(Object.assign({}, user)); // 리액트가 새 객체임을 알 수 있게 해준다.
+
+    setUserObj({
+      displayName: user.displayName,
+      uid:user.uid,
+      updateProfile: (args) => user.updateProfile(args)
+    }); // 객체의 크기가 너무 크면 리액트에서 상태를 비교하기 어려워 리렌더링 여부를 파악할 수 없다.
+  };
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} refreshUser={refreshUser} /> : "Initializing..."}
       <footer>
         &copy; Kwitter {new Date().getFullYear()}
       </footer>
     </>
   )
 }
+
 
 export default App;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { authService } from 'myFirebase';
+import React, { useEffect, useState } from 'react';
+import { authService, dbService } from 'myFirebase';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+const Profile =  ({userObj, refreshUser}) => {
         const history = useHistory();
+        const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
         const onLogOutClick = () => {
                 authService.signOut();
                 history.push('/');
         }
+        const getMyKweets = async () => {
+                const kweets = await dbService
+                .collection("kweets")
+                .where("creatorId", "==", userObj.uid)
+                .orderBy("createdAt")
+                .get();
+                console.log(kweets.docs.map((doc) => doc.data()));
+        };
+        useEffect(() => {
+                getMyKweets();
+        }, []);
+        const onChange = (event) => {
+                const {target:{value}} = event;
+                setNewDisplayName(value);
+        };
+        const onSubmit = async (event) => {
+                event.preventDefault();
+                if(userObj.displayName !== newDisplayName) {
+                        await userObj.updateProfile({
+                                displayName: newDisplayName
+                        });
+                        refreshUser();
+                }
+        };
         return (
                 <>
+                <form onSubmit={onSubmit}>
+                        <input type="text" placeholder="Display name" value={newDisplayName} onChange={onChange} />
+                        <input type="submit" value="Update Profile"/>
+                </form>
                         <button onClick={onLogOutClick}>Log out</button>
                 </>
         )
 }
+
+export default Profile;
