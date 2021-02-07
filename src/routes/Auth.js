818cab7d27bc4ee65560f6a68ab0e3b6c9b8414b
김현진 <hyunjin@gimhyeonjin-ui-MacBookAir.local>
2021-02-07 21:44:26 +0900

    code refactorying and add style.css

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,38 +1,9 @@
 import React, { useState } from 'react';
 import { authService, firebaseInstance } from 'myFirebase'
+import AuthForm from 'components/AuthForm'
 
 const Auth = () => {
-        const [email, setEmail] = useState("");
-        const [password, setPassword] = useState("");
-        const [newAccount, setNewAccount] = useState(true);
         const [error, setError] = useState("");
-        const onChange = (event) => {
-                const { target: { name, value } } = event;
-                if (name === 'email') {
-                        setEmail(value)
-                } else if (name === 'password') {
-                        setPassword(value)
-                }
-        };
-        const onSubmit = async (event) => {
-                event.preventDefault();
-                let data;
-                try {
-                        if (newAccount) {
-                                //create account
-                                data = await authService.createUserWithEmailAndPassword(email, password)
-                        } else {
-                                //log in
-                                data = await authService.signInWithEmailAndPassword(email, password)
-                        }
-                } catch (error) {
-                        setError(error.message)
-                }
-
-
-
-        };
-        const toggleAccount = () => setNewAccount((prev) => !prev);
         const onSocialClick = async (event) => {
                 const { target: { name } } = event;
                 let provider
@@ -51,15 +22,7 @@ const Auth = () => {
 
         return (
                 <div>
-                        <form onSubmit={onSubmit}>
-                                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
-                                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-                                {error}
-                        </form>
-                        <span onClick={toggleAccount}>
-                                {newAccount ? "Sign In" : "Create Account"}
-                        </span>
+                        <AuthForm />
                         <div>
                                 <button onClick={onSocialClick} name="google">Continue with Google</button>
                                 <button onClick={onSocialClick} name="github">Continue with Github</button>
@@ -67,4 +30,4 @@ const Auth = () => {
                 </div>
         );
 }
-export default Auth
+export default Auth;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { dbService, storageService } from 'myFirebase';
+import { dbService } from 'myFirebase';
 import Kweet from 'components/Kweet';
-import { v4 as uuidv4 } from 'uuid';
+import KweetFactory from 'components/KweetFactory';
 
 const Home = ({ userObj }) => {
-        const [kweet, setKweet] = useState("");
         const [kweets, setKweets] = useState([]);
-        const [attachment, setAttachment] = useState("");
         // const getKweets = async () => {
         //         const dbKweets = await dbService.collection("kweets").get();
         //         dbKweets.forEach((document) => {
@@ -26,53 +24,10 @@ const Home = ({ userObj }) => {
                         setKweets(kweetArray);
                 })
         }, [])
-        const onSubmit = async (event) => {
-                event.preventDefault();
-                let attachmentUrl = ""
-                if (attachment !== "") {
-                        const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
-                        const response = await attachmentRef.putString(attachment, "data_url");
-                        attachmentUrl = await response.ref.getDownloadURL();
-                }
-
-                const kweetObj = {
-                        text: kweet,
-                        createdAt: Date.now(),
-                        creatorId: userObj.uid,
-                        attachmentUrl
-                }
-                await dbService.collection("kweets").add(kweetObj);
-                setKweet("");
-                setAttachment("");
-        };
-        const onChange = (event) => {
-                const { target: { value } } = event;
-                setKweet(value);
-        };
-        const onFileChange = (event) => {
-                const { target: { files } } = event;
-                const theFile = files[0];
-                const reader = new FileReader();
-                reader.onloadend = (finishedEvent) => {
-                        const { currentTarget: { result } } = finishedEvent
-                        setAttachment(result);
-                }
-                reader.readAsDataURL(theFile)
-        };
-        const onClearAttachment = () => {
-                setAttachment(null)
-        }
+        
         return (
                 <>
-                        <form onSubmit={onSubmit}>
-                                <input type="text" placeholder="What's on your mind?" value={kweet} maxLength={120} onChange={onChange} />
-                                <input type="file" accept="image/*" onChange={onFileChange} />
-                                <input type="submit" value="Kweet" />
-                                {attachment && <div>
-                                        <img src={attachment} width="50px" height="50px" />
-                                        <button onClick={onClearAttachment}>Cancel</button>
-                                </div>}
-                        </form>
+                        <KweetFactory userObj={userObj} />
                         {kweets.map((kweet) => (
                                 <Kweet key={kweet.id} kweetObj={kweet} isOwner={kweet.creatorId === userObj.uid} />
                         ))}
